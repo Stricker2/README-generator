47266cc08c3e39358b13af4037a87663cd5d18a4
Stricker2 <101269390+Stricker2@users.noreply.github.com>
2022-06-26 02:25:12 -0400

    install inquierer and add question prompts

--- index.js
+++ index.js
@@ -1,13 +1,78 @@
 // TODO: Include packages needed for this application
+var inquirer = require('inquirer');
+console.log(inquirer)
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'project title',
+            message: 'What is the title of your project?'
+        },
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Write a brief description of your project:'
+        },
 
-// TODO: Create a function to initialize app
-function init() {}
+        {
+            type:'input',
+            name: 'installations',
+            message: 'Please describe how to install dependencies:'
+        },
 
-// Function call to initialize app
-init();
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'What information is pertinent for the user to know while using this project?'
+        },
+
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'How do you run tests for this project?'
+        },
+
+        {
+            type: 'input',
+            name: 'contribute',
+            message: 'How can someone contribute to this project?'
+        },
+
+        {
+            type: 'list',
+            name: 'license',
+            message: 'What license does your project have?',
+            choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'BSD 2', 'GPL', 'LGPL', 'None']
+        },
+
+        {
+            type: 'input',
+            name: 'author',
+            message: "What is the author's name?"
+        },
+
+        {
+            type: 'input',
+            name: 'username',
+            message: 'What is your GitHub username?'
+        },
+
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?'
+        }
+    ]);
+}
+
+// // TODO: Create a function to write README file
+// function writeToFile(fileName, data) {}
+
+// // TODO: Create a function to initialize app
+// function init() {}
+
+// // Function call to initialize app
+// init();
